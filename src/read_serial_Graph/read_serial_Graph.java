6de86d9454b4bc364bdd81f1820d0e2463989768
Christian <christian.s.mark@hotmail.com>
2018-06-12 09:15:13 +0200

    Java kode eksempler taget fra nettet.

---
+++ src/read_serial_Graph/read_serial_Graph.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package read_serial_Graph;
+import com.fazecast.jSerialComm.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+/**
+ *
+ * @author https://www.youtube.com/watch?v=cw31L_OwX3A
+ */
+public class read_serial_Graph {
+
+
+    static SerialPort valgteport;
+    static int x = 0;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame vindue = new JFrame();
+        vindue.setTitle("Graf over Serial input");
+        vindue.setSize(600,500);
+        vindue.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // reagér på luk
+        vindue.setLayout(new BorderLayout());
+        //vindue.pack();
+        
+        // Drop-down box og tilslut knap
+        JComboBox<String> portlist = new JComboBox<String>();
+        JButton tilslutknap = new JButton("Tilslut");
+        JPanel topPanel = new JPanel();
+        topPanel.add(portlist);
+        topPanel.add(tilslutknap);
+        vindue.add(topPanel, BorderLayout.NORTH);
+        
+        // Læg noget i drop-down boxen
+        SerialPort ports[] = SerialPort.getCommPorts();
+        
+        for (int i = 0; i < ports.length; i++) {
+            portlist.addItem(ports[i].getSystemPortName());
+        }
+        
+        // Lav Grafen
+        XYSeries series = new XYSeries("Værdier modtaget fra serial porten");
+        XYSeriesCollection dataset = new XYSeriesCollection(series);
+        JFreeChart chart = ChartFactory.createXYLineChart("Værdier modtaget fra serial porten", "Tid", "Værdier", dataset);
+        vindue.add(new ChartPanel(chart), BorderLayout.CENTER);
+        
+        
+        // Opsæt tilslut knappen og brug en anden proces til at lytte efter data
+        tilslutknap.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent ae) {
+                if(tilslutknap.getText().equals("Tilslut")){
+                    //Tilslut
+                    valgteport = SerialPort.getCommPort(portlist.getSelectedItem().toString());
+                    valgteport.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
+                    if(valgteport.openPort()){
+                        System.out.println("Porten blev opsat korrekt");
+                        tilslutknap.setText("Afbryd");
+                        portlist.setEnabled(false);
+                    }else{
+                        try {
+                            //System.out.println("");
+                            throw new Exception("Porten kunne ikke åbnes korrekt, prøv igen!");
+                        } catch (Exception ex) {
+                            System.exit(0);
+                        }
+                    }
+                    
+                    
+                    
+                    // Lav en ny thread og læg data i grafen
+                    Thread thread = new Thread(){
+                        @Override public void run(){
+                            Scanner scanner = new Scanner(valgteport.getInputStream());
+                            while(scanner.hasNextLine()){
+                                try{
+                                    String line = scanner.nextLine();
+                                    int number = Integer.parseInt(line);
+                                    series.add(x++,number);
+                                    vindue.repaint();
+                                }catch(Exception e){}
+                            }
+                            scanner.close();
+                        }
+                    };
+                    thread.start();
+                }else{
+                    //Afbryd forbindelsen med COM porten
+                    valgteport.closePort();
+                    tilslutknap.setText("Tilslut");
+                    portlist.setEnabled(true);
+                    series.clear();
+                    x = 0;
+                }
+            }
+        });
+        
+        
+        vindue.setVisible(true);
+        
+        
+    }
+
+}
---
+++ src/read_serial_port/Read_Serial_Port.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package read_serial_port;
+import com.fazecast.jSerialComm.*;
+import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.JSlider;
+
+/**
+ *
+ * @author Christian Mark
+ */
+public class Read_Serial_Port {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame vindue = new JFrame();
+        JSlider slider = new JSlider();
+        slider.setMaximum(1023);
+        vindue.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // reagér på luk
+        vindue.add(slider);
+        vindue.pack();
+        vindue.setVisible(true);
+       
+        SerialPort ports[] = SerialPort.getCommPorts();
+        System.out.println("Vælg en COM port: ");
+        
+        int i = 1;
+        for(SerialPort port : ports){
+            System.out.println(i++ + ". "+ port.getSystemPortName());
+        }
+        Scanner tastatur = new Scanner(System.in);
+        
+        int valg= 0;
+        SerialPort port = null;
+        valg = tastatur.nextInt();
+        port = ports[valg - 1];
+        
+        
+        if(port.openPort()){
+            System.out.println("Porten blev opsat korrekt");
+        }else{
+            System.out.println("Porten kunne ikke åbnes korrekt, prøv igen!");
+        }
+        
+        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+        
+        Scanner data = new Scanner(port.getInputStream());
+        while(data.hasNextLine()){
+            int nummer = 0;
+            try {
+                nummer = Integer.parseInt(data.nextLine());
+            } catch (Exception e) {
+            }
+            slider.setValue(nummer);
+        }
+    }
+
+}
